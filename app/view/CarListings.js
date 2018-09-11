126f25be28e0054b44bacf6dc2a0b5f3ef78146e
John Stratton <jstrat0407@gmail.com>
2018-09-11 19:36:34 -0400

    added detail panel

--- app/view/CarListings.js
+++ app/view/CarListings.js
@@ -21,14 +21,21 @@ Ext.define('CarListings.view.CarListings', {
         'CarListings.view.CarListingsViewModel',
         'Ext.grid.Panel',
         'Ext.view.Table',
-        'Ext.grid.column.Column'
+        'Ext.grid.column.Column',
+        'Ext.XTemplate'
     ],
 
     viewModel: {
         type: 'carlistings'
     },
     frame: true,
-    height: 250,
+    height: 500,
+    tpl: [
+        '`<img src="data/{img}" style="float: right" />',
+        'Manufacturer: {manufacturer} ',
+        'Model: <a href="{wiki}" target="_blank">{model}</a><br>',
+        'Price: {price:usMoney}`'
+    ],
     width: 400,
     title: 'Car Listing',
 
@@ -73,6 +80,11 @@ Ext.define('CarListings.view.CarListings', {
                     text: 'Quality'
                 }
             ]
+        },
+        {
+            xtype: 'panel',
+            flex: 1,
+            title: 'My Panel'
         }
     ]
 
